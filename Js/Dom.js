b476471451e575ce78e2aa4f7dcb1eb0d549b12b
RagulSid <ragulsiddharth1@gmail.com>
2022-03-26 08:06:54 +0530

    Dom -working with nodes

--- Js/Dom.js
+++ Js/Dom.js
@@ -9,7 +9,6 @@ console.log(document.all[1]);
 console.log(document.links);
 
 //Selectors
-
 //getelement by ID
 console.log(document.getElementById("header-title"));
 var headertitle = document.getElementById("header-title");
@@ -60,3 +59,52 @@ secitem.style.color = 'gold';
 var titles = document.querySelectorAll('.title');
 console.log(titles); //nodelist - similar to collections - we can use array functions 
 titles[0].textContent = 'Add items :';
+
+// var odd = document.querySelectorAll('li:nth-child(odd)');
+// var even = document.querySelectorAll('li:nth-child(even)');
+
+// for(var i=0;i<odd.length;i++){
+//     odd[i].style.backgroundColor = 'lightgrey';
+//     even[i].style.backgroundColor = 'lightgreen';
+
+// }
+
+
+//Traversing DOM - Parents,sibilings,children
+var itemlist = document.querySelector('#items');
+console.log(itemlist.parentNode);
+//itemlist.parentNode.style.backgroundColor = 'lightgrey';
+console.log(itemlist.parentNode.parentNode); // will return container where class called container
+ 
+//Parent element
+// console.log(itemlist.parentElement);
+// itemlist.parentElement.style.backgroundColor = 'lightgrey';
+// console.log(itemlist.parentElement.parentElement);
+
+//childNodes
+console.log(itemlist.childNodes);
+console.log(itemlist.children);
+console.log(itemlist.children[1]);
+
+// itemlist.children[1].textContent="hello";
+// console.log(itemlist.children[1]);
+// console.log(itemlist.nextSibling);
+// we can use first,last,previous sibling similarly 
+
+//CreateElement
+//create a div
+
+var newDiv = document.createElement('div');
+newDiv.className='hello';
+newDiv.id='hello1';
+newDiv.setAttribute('title','hello div  ');
+//console.log(newDiv);
+
+var newDivtext = document.createTextNode('hello world');
+newDiv.appendChild(newDivtext); //add text to div
+//console.log(newDiv);
+// var container = document.querySelector('header .container');
+// var h1 = document.querySelector('header h1');
+// console.log(newDiv);
+// container.insertBefore(newDiv,h1);
+// newDiv.style.fontSize = '30px';
